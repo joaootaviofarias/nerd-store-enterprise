0029b5d071bd09b56d508dfa1a56899bafb06852
João Otávio <JoaoOtavio_DFDO@hotmail.com>
2021-06-15 22:32:26 -0300

    Adicionar arquivos de projeto.

---
+++ src/Services/NSE.Identidade.API/Models/UsuarioLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Identidade.API.Models
+{
+    public class UsuarioLogin
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; }
+    }
+}
