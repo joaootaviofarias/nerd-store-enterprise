67c1a5e66cc33ce83a8a8a674096a4551e1c5c16
joaoOtavio9 <joaootavio_dfdo@hotmail.com>
2021-06-29 22:38:12 -0300

    Api catalogo

---
+++ src/Services/NSE.Catalogo.API/Data/CatalogoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Data
+{
+    public class CatalogoContext : DbContext
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) { }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
---
+++ src/Services/NSE.Catalogo.API/Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NSE.Catalogo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Data.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
