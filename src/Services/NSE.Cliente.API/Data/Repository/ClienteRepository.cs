17989446d9c64108bf7b4107265c9674c07a52f8
João Otávio <JoaoOtavio_DFDO@hotmail.com>
2021-07-04 17:12:48 -0300

    Api cliente

---
+++ src/Services/NSE.Cliente.API/Data/Repository/ClienteRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NSE.Cliente.API.Models;
+using NSE.Core.Data;
+
+namespace NSE.Cliente.API.Data.Repository
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly ClienteContext _context;
+
+        public ClienteRepository(ClienteContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public async Task<IEnumerable<Models.Cliente>> ObterTodos()
+        {
+            return await _context.Clientes.AsNoTracking().ToListAsync();
+        }
+
+        public Task<Models.Cliente> ObterPorCpf(string cpf)
+        {
+            return _context.Clientes.FirstOrDefaultAsync(c => c.Cpf.Numero == cpf);
+        }
+
+        public void Adicionar(Models.Cliente cliente)
+        {
+            _context.Clientes.Add(cliente);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
